5dbd02e9aa26440954be5a26b10282e66ed99a6b
NexusUA <pbogomolny93@gmal.com>
2022-08-02 13:11:24 +0200

    add custom fonts

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -15,34 +15,48 @@ struct RecipeListView: View {
         
         NavigationView {
             
-            VStack(alignment: .leading) {
+            VStack (alignment: .leading) {
                 Text("All Recipes")
-                    .fontWeight(.bold)
+                    .bold()
                     .padding(.top, 40)
-                    .font(.largeTitle)
-                                
+                    .font(Font.custom("Avenir Heavy", size: 24))
+                
                 ScrollView {
-                    LazyVStack(alignment: .leading) {
+                    LazyVStack (alignment: .leading) {
                         ForEach(model.recipes) { r in
-                        
-                        NavigationLink(
-                            destination: RecipeDetailView(recipe: r),
-                            label: {
-                                HStack(spacing: 20.0) {
-                                    Image(r.image)
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 50, height: 50, alignment: .center)
-                                        .clipped()
-                                        .cornerRadius(5)
+                            
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe:r),
+                                label: {
                                     
-                                    Text(r.name)
-                                        .foregroundColor(.black)
-                                }
-                            })
-                    }
+                                    // MARK: Row item
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        
+                                        VStack (alignment: .leading) {
+                                            Text(r.name)
+                                                .foregroundColor(.black)
+                                                .font(Font.custom("Avenir Heavy", size: 16))
+                                            
+                                            RecipeHighlights(highlights: r.highlights)
+                                                .foregroundColor(.black)
+                                        }
+                                    }
+                                    
+                                })
+                            
+                            
+                            
+                        }
                     }
                 }
+                
+                
             }
             .navigationBarHidden(true)
             .padding(.leading)
@@ -50,7 +64,7 @@ struct RecipeListView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
             .environmentObject(RecipeModel())
